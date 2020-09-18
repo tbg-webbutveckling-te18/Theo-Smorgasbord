e6b76c14447fa77452d12b1d68d4cf85eedade31
Theo Holmgren <theokodar@gmail.com>
2020-09-18 15:03:32 +0200

    First lesson with JS

---
+++ Webbutveckling 2/2-JavaScript/assets/js/1-ifelse.js
@@ -0,0 +1,24 @@
+//Greater than
+var age = 17;
+if (age >= 18) {
+    console.log('Du får ta körkortet.');
+} else {
+    console.log('Du får inte ta körkortet.');
+}
+
+// If something is equal to
+var lesson = 'Web development';
+if (lesson == 'History') {
+    console.log('We have: ' + lesson);
+} else if (lesson == 'Web development') {
+    console.log('We have ' + lesson);
+} else {
+    console.log('Spare time!');
+}
+
+var isSunny = false;
+if (isSunny == true) {
+    console.log('Put on some shorts today.');
+} else {
+    console.log('Put on some warm clothes today');
+}
