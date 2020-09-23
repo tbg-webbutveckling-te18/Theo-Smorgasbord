9df35dc73675b824f79dda2835f08dde27503dde
Theo Holmgren <theokodar@gmail.com>
2020-09-23 11:50:32 +0200

    Worked oon Ifelse.JS

--- Webbutveckling 2/2-JavaScript/assets/js/1-ifelse.js
+++ Webbutveckling 2/2-JavaScript/assets/js/1-ifelse.js
@@ -22,3 +22,32 @@ if (isSunny == true) {
 } else {
     console.log('Put on some warm clothes today');
 }
+
+// step Uno
+var d = new Date();
+
+if (d.getHours() < 18) {
+    console.log('Goodday sir!')
+
+    //step Dos
+} else if (d.getHours() < 10) {
+    console.log('Good Morning')
+} else {
+    console.log('Good evening boys and girls!')
+}
+
+//step Tres
+if (d.getHours() < 11) {
+    console.log('Vakna nu, din sopa!')
+
+    if(d.getMonth() > 9 || d.getMonth() < 2) {
+        console.log('Sov som en björn')
+    }
+    
+} else if (d.getHours() < 10) {
+    console.log('Good Morning')
+} else {
+    console.log('Good evening boys and girls!')
+}
+
+
