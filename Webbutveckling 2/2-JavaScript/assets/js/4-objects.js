efac0fe8c8a17841350b70656b671252a07207e0
Theo Holmgren <theokodar@gmail.com>
2021-01-27 11:47:02 +0100

    Objects and stuff

--- Webbutveckling 2/2-JavaScript/assets/js/4-objects.js
+++ Webbutveckling 2/2-JavaScript/assets/js/4-objects.js
@@ -12,12 +12,36 @@ var user = {
     age: 28,
     city: "HereSneezedDuck",
     friends: ["Bob", "Ross"],
-    isEvil: true
-    // shoutout: function() {
-    //     alert("Shoutout to the Souls Of Mischief crew!")
+    isEvil: true,
+    shoutout: function() {
+        alert("Shoutout to the Souls Of Mischief crew!");
     }
 };
 
 document.writeln(` <br> </br> My name is ${user.name} and I am ${user.age} years old. I live in ${user.city}`)
 
 user.shoutout();
+
+
+var posts = [
+    {
+        author: "Phesto",
+        comment: "I be coolin', school's in session, but I'm fresh in Rappin, so I take time off to never rhyme soft"
+    
+        
+    },
+    {
+        author: "Big L",
+        comment: "If rap was a game I'd be MVP, the most valuable poet in the NYC"
+    },
+    {
+        author: "Kareem Campbell",
+        comment: "Yo dude, chill"
+    }
+]
+var postOutput = document.querySelector(".posts");
+
+
+posts.forEach(function(post) {
+    postOutput.innerHTML += `Author: ${post.author} <br> Commented: ${post.comment} <br><br>`;
+})
