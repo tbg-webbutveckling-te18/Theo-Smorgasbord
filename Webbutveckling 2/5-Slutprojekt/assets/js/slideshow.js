c419c19a914ec908b842b7542bbd45b942f711a9
Theo Holmgren <theokodar@gmail.com>
2021-05-26 11:45:40 +0200

    Worked on slideshow

--- Webbutveckling 2/5-Slutprojekt/assets/js/slideshow.js
+++ Webbutveckling 2/5-Slutprojekt/assets/js/slideshow.js
@@ -1,9 +1,11 @@
 
-var slideshow = document.getElementsByClassName("slideshow")[0];
+// var slideshow = document.getElementsByClassName("slideshow")[0];
+
+var cards = document.getElementsByClassName("card")[0];
 
 var btnLeft = document.getElementById("left");
 btnLeft.addEventListener("click", function(){
-    slideshow.style.marginLeft = "-250px";
+    cards.style.marginLeft = "-250px";
 })
 
 
@@ -11,6 +13,6 @@ btnLeft.addEventListener("click", function(){
 
 var btnRight = document.getElementById("right");
 btnRight.addEventListener("click", function(){
-    slideshow.style.marginLeft = "250px";
+    cards.style.marginLeft = "250px";
 })
 
